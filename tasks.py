11ba15e2b737ecba7d512a6e5996186eeabc5e9f
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-08 16:14:53 -0600

    Import sounds from acoustic-similarity project

--- tasks.py
+++ tasks.py
@@ -1,6 +1,26 @@
 from invoke import task
+from unipath import Path
+
+from run import Trials
+
 
 @task
 def select_sounds(ctx):
     """Select the sounds to use in this experiment."""
     ctx.run('Rscript R/select_messages.R')
+
+@task
+def copy_sounds(ctx):
+    """Copy sounds from acoustic-similarity to use in this experiment."""
+    src_dir = Path('../acoustic-similarity/data/sounds')
+    dst_dir = Path('stimuli/sounds')
+
+    if not dst_dir.isdir():
+        dst_dir.mkdir()
+
+    trials = Trials()
+    for seed_id in trials.messages.seed_id.unique():
+        seed_name = '{}.wav'.format(seed_id)
+        dst = Path(dst_dir, seed_name)
+        if not dst.exists():
+            Path(src_dir, seed_name).copy(dst)
